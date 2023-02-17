ee61ed097ec047afde70e06e9ae3d92c64d4cb4d
paragonyun <paragonyun@naver.com>
2023-02-17 22:05:25 +0900

    model

--- RNN_TEXT_GENERATION/model.py
+++ RNN_TEXT_GENERATION/model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 """https://www.tensorflow.org/text/tutorials/text_generation"""
 class MyModel(tf.keras.Model):
     def __init__(self, 
-                vocab_size: int = 65, 
+                vocab_size: int = 66, 
                 embedding_dim: int = 256, 
                 rnn_units: int = 1024):
         super(MyModel, self).__init__()
@@ -36,3 +36,44 @@ class MyModel(tf.keras.Model):
         else:
             return output
         
+
+class Generator(tf.keras.Model):
+    def __init__(self, model, chars_from_ids, ids_from_chars, temperature):
+        super(Generator, self).__init__()
+
+        self.temperature = temperature
+        self.model = model
+        self.chars_from_ids = chars_from_ids
+        self.ids_from_chars = ids_from_chars
+
+        # MASK를 생성합니다.
+        skip_ids = self.ids_from_chars(['UNK'])[:, None]
+        sparse_mask = tf.SparseTensor(
+                    values=[-float('inf')] * len(skip_ids), # ids 수만큼 -inf 생성
+                    indices=skip_ids,
+                    dense_shape=[len(ids_from_chars.get_vocabulary())]
+        )
+
+        self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+
+    @tf.function
+    def generate_one_step(self, inputs, states=None):
+        input_chars = tf.strings.unicode_split(inputs, "UTF-8")
+        input_ids = self.ids_from_chars(input_chars).to_tensor()
+
+        predicted_logits, states = self.model(inputs=input_ids, 
+                                                states=states,
+                                                return_state=True)
+        
+        # 마지막 예측값만 사용
+        predicted_logits = predicted_logits[:, -1, :] # 자동으로 list에 넣어주는듯
+        predicted_logits = predicted_logits/self.temperature
+
+        predicted_logits = predicted_logits + self.prediction_mask
+
+        predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+        predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+
+        predicted_chars = self.chars_from_ids(predicted_ids)
+
+        return predicted_chars, states
